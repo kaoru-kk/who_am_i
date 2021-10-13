4f059092ef48b720e96e0ee8798fdbbfd840238d
ksoru-kk <kkaoru1998@gmail.com>
2021-10-13 15:19:50 +0900

    img to Image

--- components/Works.js
+++ components/Works.js
@@ -1,15 +1,16 @@
+import Image from "next/image";
 
 export default function Works() {
-
   return (
     <section id="works">
       <h1>works</h1>
 
       <div data-scroll data-scroll-speed="4" className="picture-container">
-        <img
+        <Image 
+          src='/images/wankoinsoba.jpg'
           className="picture"
-          src="https://picsum.photos/800/500"
-          alt="tadaam"
+          width={100}
+          height={100}
         />
       </div>
     </section>
