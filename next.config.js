f1b5a2a8bdafa66f33996b8902f269a5df4d696f
ksoru-kk <kkaoru1998@gmail.com>
2021-10-13 16:57:34 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
 module.exports = {
-  reactStrictMode: true,
-}
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
+};
