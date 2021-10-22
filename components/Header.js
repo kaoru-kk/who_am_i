51963d0b51ec4bc41e667c8e77a31d80666b49e6
ksoru-kk <kkaoru1998@gmail.com>
2021-10-22 17:57:49 +0900

    ogp fi

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,13 @@ export default function Header() {
   return (
     <Head>
       <title>Kaoru&apos;s Portfolio</title>
+      <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <meta property="og:title" content="Kaoru&apos;s Portfolio" />
       <meta property="og:site_name" content="Kaoru&apos;s Portfolio" />
-      <meta property="og:image" content="https://who-am-i-wan-k.vercel.app/images/ogp.jpg" />
+      <meta property="og:title" content="Kaoru&apos;s Portfolio" />
+      <meta property="og:description" content="My name is Kaoru k. i&apos;m softwaer enginerr!" />
+      <meta property="og:url" content="https://who-am-i-k4oru.vercel.app/images/ogp.jpg" />
+      <meta property="twitter:image" content="https://who-am-i-k4oru.vercel.app/images/ogp.jpg" />
     </Head>
   )
 }
