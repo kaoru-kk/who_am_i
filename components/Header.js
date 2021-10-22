1b82665d3151b69a847e84e9f49b88e22260e061
ksoru-kk <kkaoru1998@gmail.com>
2021-10-22 18:07:31 +0900

    誤字＞＜

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <meta property="og:site_name" content="Kaoru&apos;s Portfolio" />
       <meta property="og:title" content="Kaoru&apos;s Portfolio" />
-      <meta property="og:description" content="My name is Kaoru k. i&apos;m softwaer enginerr!" />
+      <meta property="og:description" content="My name is Kaoru k. i&apos;m software enginerr!" />
       <meta property="og:url" content="https://who-am-i-k4oru.vercel.app/" />
       <meta property="og:image" content="https://who-am-i-k4oru.vercel.app/images/ogp.jpg" />
       <meta property="twitter:image" content="https://who-am-i-k4oru.vercel.app/images/ogp.jpg" />
