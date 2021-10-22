5584f35449dd691631d763fc6f124c58039c55f5
ksoru-kk <kkaoru1998@gmail.com>
2021-10-22 17:47:12 +0900

    fix single quotation

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
-// import Image from "next/image"
 
 export default function Header() {
   return (
     <Head>
-      <title>Kaoru's Portfolio</title>
+      <title>Kaoru&apos;s Portfolio</title>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <meta property="og:title" content="Kaoru's Portfolio" />
-      <meta property="og:site_name" content="Kaoru's Portfolio" />
+      <meta property="og:title" content="Kaoru&apos;s Portfolio" />
+      <meta property="og:site_name" content="Kaoru&apos;s Portfolio" />
       <meta property="og:image" content="https://who-am-i-wan-k.vercel.app/images/ogp.jpg" />
     </Head>
   )
