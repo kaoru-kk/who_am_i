afd309c8656cd4cd1efd78afadaa442579fd69f3
ksoru-kk <kkaoru1998@gmail.com>
2021-10-25 17:30:17 +0900

    add logo

--- templates/Header.js
+++ templates/Header.js
@@ -6,6 +6,7 @@ export default function Header() {
       <title>Kaoru&apos;s Portfolio</title>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <link rel="shortcut icon" href="https://who-am-i-k4oru.vercel.app/images/logo.png" type="image/png" />
       <meta property="og:site_name" content="Kaoru&apos;s Portfolio" />
       <meta property="og:title" content="Kaoru&apos;s Portfolio" />
       <meta property="og:description" content="My name is Kaoru k. i&apos;m software enginerr!" />
