376988f51fb557770f05b14af818e3edbfa69c7a
ksoru-kk <kkaoru1998@gmail.com>
2021-11-25 09:44:46 +0900

    fix career

--- organisms/Career.js
+++ organisms/Career.js
@@ -5,8 +5,8 @@ function Data() {
   return (
     [
       {period: '2016.04 - 2020.03', place: 'KGU SPS', detail: 'university,  Media and Information Studies'},
-      {period: '2018.04 - 2020.01', place: 'infratop.inc', detail: 'mentor and engineer intern at DMM WEBCAMP'},
-      {period: '2020.05 - now', place: 'PECO.inc', detail: 'pet company. i love dog!'}
+      {period: '2018.04 - 2020.09', place: 'infratop.inc', detail: 'mentor and engineer intern at DMM WEBCAMP'},
+      {period: '2020.05 - now', place: 'PECO.inc', detail: 'system engineer (RoR, Vue.js)'}
     ]
   );
 };
